99d2d833ca25b0832bbf761400eef99e6caf2ffd
Salvarez0212 <alvarezsebastian31@gmail.com>
2023-04-28 21:28:39 -0500

    deleted unnecessary console.log

--- src/api/lists/lists.service.ts
+++ src/api/lists/lists.service.ts
@@ -3,8 +3,6 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const getAllLists = (userId: string) => {
-  console.log(userId);
-
   return prisma.list.findMany({
     where: {
       userId: { equals: userId },
